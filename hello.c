58f1ba7de629ad93db2fa40b4dd73877ae7b7f3d
Adele Keener <akeener3@huskers.unl.edu>
2025-08-28 12:52:01 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Adele Keener
+ *         akeener3@huskers.unl.edu
+ * Date: 2025/08/26
+ *
+ * Basic hello world program
+ *
+ */
+
+int main(int argc, char **argv) {
+
+  printf("Hello, I'm Adele Keener!")
+  printf("Major is Computer Science!")
+
+  return 0;
+}
